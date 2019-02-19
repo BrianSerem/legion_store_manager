f7ea79e5da5f4d20aff8e388708cbd15688ef2a6
BrianSerem <brianseremiat@gmial.com>
2019-02-19 20:49:21 +0300

    [Chore #164071426] Creation of User model.

---
+++ store_manager/apps/authentication/models.py
@@ -0,0 +1,28 @@
+from datetime import datetime, timedelta
+from django.conf import settings
+
+from django.db import models
+
+
+class User():
+  
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    role = models.EmailField(db_index=True, unique=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+
+    def __str__(self):
+       
+        return self.email
+
+    @property
+    def get_full_name(self):
+       
+        return self.username
+
+    def get_short_name(self):
+       
+        return self.username
