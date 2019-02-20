8d9fa8c8be7092f59e65c8d9017ed4b74ffcc6d8
MandelaK <kazi.keith@gmail.com>
2019-02-20 13:57:39 +0300

    chore(models): create sales model
    Create sales model that will be a representation of the sales instance.
    
    [starts #164071475]

--- store_manager/apps/sales/models.py
+++ store_manager/apps/sales/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.utils.text import slugify
 
@@ -44,3 +45,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+        
+class TimeStamped(models.Model):
+    """
+    Creation time and update timestamps
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Sale(TimeStamped):
+    """This class defines a sale instance"""
+    attendant = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
