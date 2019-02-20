1956a0e21f54866d8e2b2bade622a7590a6cc8d1
MandelaK <kazi.keith@gmail.com>
2019-02-20 13:59:53 +0300

    chore(model tests): add tests for sales model
    Add tests to check if tests pass
    
    [finishes #164071475]

--- store_manager/apps/sales/tests.py
+++ store_manager/apps/sales/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
+from .factories import (CategoryFactory, ProductFactory, SaleFactory,
+                        SaleItemFactory, UserFactory)
+
+
 # Create your tests here.
+
+class SaleModelTests(TestCase):
+
+    def setUp(self):
+        self._category1 = CategoryFactory.create()
+        self._product1 = ProductFactory.create(category=self._category1)
+        self._user = UserFactory.create()
+        self._sale = SaleFactory.create(attendant=self._user)
+
+    def test_owner_can_add_attendant_to_sale(self):
+        sale_test = SaleFactory.create(attendant=self._user)
+        self.assertEqual(sale_test.attendant, self._user)
+
+        
