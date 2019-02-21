cc77b110d5acca1f74d501d3c87dcef38e71d8d1
James Etole <jamesetole@Jamess-MacBook-Pro-3.local>
2019-02-21 04:09:15 +0300

    [Chore #164071465] create category and its assosiated tests

--- store_manager/apps/sales/models.py
+++ store_manager/apps/sales/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Category():
-    pass
+class Category(models.Model):
+    title = models.CharField(db_index=True, unique=True,max_length=200)
+    category_description = models.CharField(blank=True,max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- store_manager/apps/sales/tests.py
+++ store_manager/apps/sales/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Category
+from .factories import CategoryFactory2
+
+class CategoryTestCase(TestCase):
+    def test_add_category(self):
+        category2 = CategoryFactory2.create()
+        self.assertEqual(category2.title,'Detergent')
+
+        
