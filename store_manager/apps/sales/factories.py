23eee2848b4eb9dfad9d1227d9ba22ffc81a65ba
James Etole <jamesetole@Jamess-MacBook-Pro-3.local>
2019-02-21 09:37:44 +0300

    [Chore #164071465] Further refactoring on category model and factory.

--- store_manager/apps/sales/factories.py
+++ store_manager/apps/sales/factories.py
@@ -2,7 +2,7 @@ from factory import DjangoModelFactory, Faker
 
 from .models import Category
 
-class CategoryFactory2(DjangoModelFactory):
+class CategoryFactory(DjangoModelFactory):
     title = Faker('Detergent')
     category_description = Faker('This category holds washing soaps')
 
