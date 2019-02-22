517fbb62f955700256687d0a0c73ac49dae71e89
Eric Ndung'u <eric.ndungu@andela.com>
2019-02-22 11:29:19 +0300

    product create and list view

--- store_manager/apps/sales/views.py
+++ store_manager/apps/sales/views.py
@@ -9,7 +9,3 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.products.all()
     permission_classes = (permissions.IsAuthenticated,)
     serializer_class = ProductSerializer
-
-    def create(self, *args, **kwargs):
-        user = self.request.user
-        super().create(*args, **kwargs)
